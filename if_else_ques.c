870fb67cc244f15b48845fd3d61a7c8e50bae1b4
Yash Tripathi <114800027+Yash926@users.noreply.github.com>
2023-03-03 00:13:55 +0530

    If else_questions

---
+++ if_else_ques.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+//int main()
+//{
+////	int x=5;
+////	if(x<1)
+////		printf("Hello");
+////	if(x==5)
+////		printf("hi");
+////	else
+////		printf("n0");
+////	return 0;
+////	
+////
+//	int x=0;
+//	if(x==0)
+//		printf("hi");
+//	else
+//		printf("How are you");
+//		printf("hello");
+//	return 0;
+//}
+//int main()
+//{
+//	int x=0;
+//	if(x==1)
+//		if(x>=0)
+//			printf("True\n");
+//		else
+//			printf("False\n");
+//	return 0;
+//}
+
+
+int main()
+{
+	int x=0;
+	if(x==1)
+		if(x==0)
+			printf("inside if\n");
+		else
+			printf("inside else if\n");
+	else
+		printf("inside else\n");
+	return 0;
+}
