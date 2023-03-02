ccdf844bf962f43d7d1dfca0efb510f707e6431c
Yash Tripathi <114800027+Yash926@users.noreply.github.com>
2023-03-03 00:02:49 +0530

    First Program

---
+++ first_program.c
@@ -0,0 +1,266 @@
+//#include<stdio.h>
+//int main()
+//{
+//	printf("Hello world");
+//	return 0;
+//}
+
+
+
+// Online C compiler to run C program online
+//#include <stdio.h>
+//int main()
+//{
+//int a=20;
+//float pi=3.14;
+//char c='A';
+//double d=3.844344;
+//printf("%i \n",a);
+//printf("%f \n",pi);
+//printf("%c \n",c);
+//printf("%lf",d);
+//
+//return 0;
+//}
+
+//write a program to check whether a person is eligible to vote or not ,take dynamic input.
+////#include<stdio.h>
+//int main()
+//{
+//	int age;
+//	scanf("%d",&age);
+//	if(age>=18)
+//	{
+//		printf("Person is eligible to vote");
+//	}
+//	else
+//	{
+//		printf("not eligible to vote");
+//	}
+	
+
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int x=1,y=0,z=5;
+////	int a=x&&y||z++;
+//	int a=x&&y&&z++;
+//	printf("%d",z);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//int a=5;
+//int b=5;
+//printf("%d+%d=%d\n",a,b,(a+b));
+//printf("%d-%d=%d\n",a,b,(a-b));
+//printf("%d*%d=%d\n",a,b,(a*b));
+//printf("%d/%d=%d\n",a,b,(a/b));
+//printf("%d %% %d=%d\n",a,b,(a%b));
+
+
+//return 0;
+//}
+
+
+
+
+
+
+//#include<stdio.h>
+//int main(){
+//int t;
+//float p,r,si;
+//printf("Enter the Principle Value: ");
+//scanf("%f",&p);
+//printf("Enter the Time: ");
+//scanf("%d",&t);
+//printf("Enter the Rate: ");
+//scanf("%f",&r);
+//si=(p*r*t)/100;
+//printf("Simple Interest is %f whose principle value is %f and the rate is %f,and the time is %d",si,p,r,t);
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int r,d;
+//	float dist;
+//	printf("Enter radius:");
+//	scanf("%d",&r);
+//	d=2*r;
+//	dist=3.14*d;
+////	printf("Distance traveled=%f",dist);
+//	printf("Distance travelled is %f having radius %d with diameter %d" ,dist,r,d);
+//	
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int nc,ns;
+//	printf("Enter no of chocolates to be ditributed:");
+//	scanf("%d",&nc);
+//	print("Enter no. of students:");
+//	scanf("%d",&ns);
+//	
+//	printf("so %d chocolate will be left as all %d student will have %d chocolates each",(nc%ns),ns,(nc/ns);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i=3;
+//	int l=i/-2;
+//	int k=i%-2;
+//	
+//	printf("%d %d\n",l,k);
+//	
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int num;
+//	scanf("%d",&num);
+//	(num%5==0)?printf("number is divisible"):printf("number is not divisible");
+//	return 0;
+//}
+//
+
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int a=2;
+//	printf("%d",(a<<1));
+////	printf("%d",(1101<<1));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d",sizeof(char));
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("Enter a number:");
+//	scanf("%d",&a);
+//	if(a>10)
+//		
+//			printf("%d is greater than 10",a);
+//		
+//	printf("hi");
+//	return 0;
+//}
+
+
+// #include<stdio.h>
+// int main()
+// {
+// 	char ch;
+// 	char s[90];
+// 	char sen[90];
+// 	scanf("%c",&ch);
+// 	scanf("%s",&s);
+// 	scanf("%[^\n]%*c",sen);
+// 	printf("%c\n",ch);
+// 	printf("%s\n",s);
+// 	printf("%s\n",sen);
+// 	return 0;
+// }
+
+
+
+// a program to check whether a number is divisible by 5;
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("Enter a number:");
+//	scanf("%d",&a);
+//	if(a%5==0)
+//	{
+//		printf("%d is divisible by 5",a);
+//	}
+//	else
+//	{
+//		printf("%d is not divsible by 5",a);
+//	}
+//	return 0;
+//}
+
+
+// write a program to check which number is greater out of the three inputs
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a,b,c;
+//	printf("Enter a:");
+//	scanf("%d",&a);
+//	printf("Enter b:");
+//	scanf("%d",&b);
+//	printf("Enter c:");
+//	scanf("%d",&c);
+//	if(a>b&&a>c)
+//	{
+//		printf("%d is greater than %d and %d",a,b,c);
+//	}
+//	else if(b>a&&b>c)
+//	{
+//		printf("%d is greater than %d and %d",b,a,c);
+//	}
+//	else if(c>a&&c>b)
+//	{
+//		printf("%d is greater than %d and %d",c,a,b);
+//	}
+//	else
+//	{
+//		printf("They all are equal to each other");
+//	}
+//	return 0;
+//
+//}
+
+
+#include<stdio.h>
+int main()
+{
+	int x=5;
+	if(x<1)
+		printf("Hello");
+	if(x==5)
+		printf("hi");
+	else
+		printf("n0");
+	return 0;
+	
+}
+
+
+
+
+
+
+
+
