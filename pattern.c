5554c2ce967e0ab7537d4d6e7e9c575717aba207
Yash Tripathi <114800027+Yash926@users.noreply.github.com>
2023-03-08 22:50:34 +0530

    pattern

---
+++ pattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=1;j++)
+		{
+			//printf("* ");
+			//printf("%d",i);
+			printf("%d",j);
+
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
+//#include<stdio.h>
+//int main()
+//{
+//	int i,j;
+//	for 
+//}
